91011b6a22c2488d921bf5bddd90bd69ff3be502
Claudio0101-bit <salgadoclaudio2005@gmail.com>
2025-01-08 19:57:47 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -4,15 +4,20 @@ from PPlay.mouse import *
 from PPlay.gameimage import *
 from PPlay.sprite import *
 from PPlay.animation import *
+from player import Player
+from goblin import Goblin
 
 
 
 # Definição da Função JOGO
 def jogo(janela):
 
-    #agua = GameImage("Tiny Swords (Update 010)/Terrain/Water/Water.png")
 
-    cena = GameImage("Assets Resized/map.png")
+    agua = GameImage("Assets Resized/mapa-agua.png")
+    terra = GameImage("Assets Resized/mapa-terra.png")
+
+    terra.x = 64 * 2
+    terra.y = 64 * 1
 
     # Definição de Variáveis de Controle
     teclado = Window.get_keyboard()         #Teclado
@@ -20,7 +25,7 @@ def jogo(janela):
     voltar = False                          #Voltar à Tela inicial
     vel = 300                               #Velocidade dos personagens
 
-    idle = ("Warrior_Blue_FILAS/Warrior_Blue_Parado_Right_1x1.png",
+    idle = ("Warrior_Blue_FILAS/Warrior_Blue_Parado_1x1.png",
             "Warrior_Blue_FILAS/Warrior_Blue_Parado_Left_1x1.png")
 
     walking = ("Warrior_Blue_FILAS/Warrior_Blue_Correndo_2x1.png",
@@ -30,145 +35,36 @@ def jogo(janela):
              ("Warrior_Blue_FILAS/Warrior_Blue_Atk-2-RIGHT_4x1.png", "Warrior_Blue_FILAS/Warrior_Blue_Atk-2-LEFT_4x1.png"))
 
     # Variáveis com "_g" referentes ao Goblin
-    idle_g = ("Torch_Red_Goblin_FILAS/Torch_Red_Parado_RIGHT_1x1.png",
-            "Torch_Red_Goblin_FILAS/Torch_Red_Parado_LEFT_1x1.png")
+    idle_g = ("Goblin_Red_FILAS/Parado_Right.png", "Goblin_Red_FILAS/Parado_Left.png")
 
-    walking_g = ("Torch_Red_Goblin_FILAS/Torch_Red_Right_Correndo_2x1.png",
-               "Torch_Red_Goblin_FILAS/Torch_Red_Left_Correndo_2x1.png")
+    walking_g = ("Goblin_Red_FILAS/Andando_Right.png", "Goblin_Red_FILAS/Andando_Left.png")
 
-    atack_g = ("Torch_Red_Goblin_FILAS/Torch_Red_Atk-RIGHT_3x1.png", "Torch_Red_Goblin_FILAS/Torch_Red_Atk-LEFT_3x1.png")
+    atack_g = ("Goblin_Red_FILAS/Atacando_Right.png", "Goblin_Red_FILAS/Atacando_Left.PNG")
 
-    #Sprite do Player/Warrior
-    player = Sprite(idle[0], 6)
-    player.set_sequence_time(0, 1152, 100, True)
-    player.set_position(janela.width/2 - 200, janela.height/2 - 200)
+    vida_tuple = ("GameAssets Goblin-Slayer/Barras-Buttons-Etc/rhombusMeters 64x16/redMeter1.png",
+                  "GameAssets Goblin-Slayer/Barras-Buttons-Etc/rhombusMeters 64x16/redMeter2.png",
+                  "GameAssets Goblin-Slayer/Barras-Buttons-Etc/rhombusMeters 64x16/redMeter3.png",
+                  "GameAssets Goblin-Slayer/Barras-Buttons-Etc/rhombusMeters 64x16/redMeter4.png",
+                  "GameAssets Goblin-Slayer/Barras-Buttons-Etc/rhombusMeters 64x16/redMeter5.png")
+    barra_vida = Sprite(vida_tuple[4])
+    barra_vida.x = 100
+    barra_vida.y = 100
 
-    #Sprite do Inimigo/Goblin com tocha
-    goblin = Sprite(idle[0], 7)
-    goblin.set_sequence_time(0, 1344, 100, True)
-    goblin.set_position(1000, 600)
+    #Sprite do Player/Warrior e do Goblin
+    player = Player(idle, walking, atack, janela.width, janela.height)
+    goblin = Goblin(idle_g, walking_g, atack_g, janela.width, janela.height)
 
-    tempo_animacao = 50/6
-    tempo_animacao_g = 50/7
-    tempo_ataque = 50 / 6
-    direcao = 0
-    direcao_g = 0
-    cooldown_forte = 0
-    cooldown_fraco = 0
-    atacou_forte = False
-    atacou_fraco = False
 
     # GAME Loop do JOGO
-
     while True:
-        cena.draw()
-        # Condicional para Voltar à tela anterior
-        if teclado.key_pressed("esc"):
-            voltar = True
-        if voltar == True:
-            voltar = False
-            break;
+        agua.draw()
+        terra.draw()
+        barra_vida.draw()
 
-        '''for i in range(0, janela.width, agua.width):
-            for j in range(0, janela.height, agua.height):
-                agua.x = i
-                agua.y = j
-                agua.draw()'''
-
-        if teclado.key_pressed("d") and player.x < janela.width - player.width:
-            direcao = 0
-            player.image = pygame.image.load(walking[direcao]).convert_alpha()
-            player.move_x(vel * janela.delta_time())
-
-        if teclado.key_pressed("a") and player.x >= 0:
-            direcao = 1
-            player.image = pygame.image.load(walking[direcao]).convert_alpha()
-            player.move_x(vel * janela.delta_time() * (-1))
-
-        if teclado.key_pressed("s") and player.y <= janela.height - player.height:
-            player.image = pygame.image.load(walking[direcao]).convert_alpha()
-            player.move_y(vel * janela.delta_time())
-
-        if teclado.key_pressed("w") and player.y >= 0:
-            player.image = pygame.image.load(walking[direcao]).convert_alpha()
-            player.move_y(vel * janela.delta_time() * (-1))
-
-        cooldown_forte -= janela.delta_time()
-        cooldown_fraco -= janela.delta_time()
-
-        if teclado.key_pressed("e"):
-            atacou_forte = True
-        if atacou_forte and cooldown_forte <= 0:
-            player.image = pygame.image.load(atack[0][direcao]).convert_alpha()
-            cooldown_forte = 1.5
-            atacou_forte = False
-
-        if teclado.key_pressed("q"):
-            atacou_fraco = True
-        if atacou_fraco and cooldown_fraco <= 0:
-            player.image = pygame.image.load(atack[1][direcao]).convert_alpha()
-            cooldown_fraco = 0.5
-            atacou_fraco = False
-        '''if teclado.key_pressed("q") and cooldown_forte <= 0:
-            while tempo_ataque > 0:
-                player.image = pygame.image.load(atack[0][direcao]).convert_alpha()
-                tempo_ataque -= (100 / 6) * janela.delta_time()
-            tempo_ataque = 50/6
-            cooldown_forte = 1.5
-
-        if teclado.key_pressed("e") and cooldown_fraco <= 0:
-
-            while tempo_ataque > 0:
-                player.image = pygame.image.load(atack[1][direcao]).convert_alpha()
-                tempo_ataque -= (100 / 6) * janela.delta_time()
-            tempo_ataque = 50 / 6
-            cooldown_fraco = 0.5'''
-
-        if (not teclado.key_pressed("w") and not teclado.key_pressed("a") and not teclado.key_pressed("s") and
-                                            not teclado.key_pressed("d") and not teclado.key_pressed("q")
-                                            and not teclado.key_pressed("e")):
-            player.image = pygame.image.load(idle[direcao]).convert_alpha()
-
-
-        # Movimentação do Goblin, ainda sem atacar
-        if teclado.key_pressed("right") and goblin.x < janela.width - goblin.width:
-            direcao_g = 0
-            goblin.image = pygame.image.load(walking_g[direcao_g]).convert_alpha()
-            goblin.move_x(vel * janela.delta_time())
-
-        if teclado.key_pressed("left") and goblin.x >= 0:
-            direcao_g = 1
-            goblin.image = pygame.image.load(walking_g[direcao_g]).convert_alpha()
-            goblin.move_x(vel * janela.delta_time() * (-1))
-
-        if teclado.key_pressed("down") and goblin.y <= janela.height - goblin.height:
-            goblin.image = pygame.image.load(walking_g[direcao_g]).convert_alpha()
-            goblin.move_y(vel * janela.delta_time())
-
-        if teclado.key_pressed("up") and goblin.y >= 0:
-            goblin.image = pygame.image.load(walking_g[direcao_g]).convert_alpha()
-            goblin.move_y(vel * janela.delta_time() * (-1))
-
-        if (not teclado.key_pressed("up") and not teclado.key_pressed("right") and not teclado.key_pressed("down") and
-                                            not teclado.key_pressed("left")):
-            goblin.image = pygame.image.load(idle_g[direcao_g]).convert_alpha()
-
-        if tempo_animacao <= 0:
-            player.stop()
-            goblin.stop()
-            tempo_animacao = 50/6
-            tempo_animacao_g = 50/7
-
-        tempo_animacao -= (100/6) * janela.delta_time()
-        tempo_animacao_g -= (100/7) * janela.delta_time()
-
-        player.play()
-        goblin.play()
-
-        player.update()
-        goblin.update()
-
-        player.draw()
-        goblin.draw()
-
+        player.run(janela.width, janela.height, vel, janela.delta_time())
+        player.attack_action(janela.delta_time())
+        player.play_n_drawn(janela.delta_time())
+        goblin.run(janela.width, janela.height, vel, janela.delta_time())
+        goblin.attack_action(janela.delta_time())
+        goblin.play_n_drawn(janela.delta_time())
         janela.update()
